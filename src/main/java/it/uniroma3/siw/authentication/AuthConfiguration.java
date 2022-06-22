a51a02c9f839b8628fd1bbccdf33da99e54a9087
Zhou Long <lon.zhou@stud.uniroma3.it>
2022-06-22 22:09:10 +0200

    part ot html

--- src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -40,8 +40,8 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
                 // authorization paragraph: qui definiamo chi può accedere a cosa
                 .authorizeRequests()
                 // chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css e alle immagini
-                .antMatchers(HttpMethod.GET, "/", "/index", "/login", "/register", "/css/**", "/images/**", "/userChefs", "/userChef/**", 
-                		 "/userBuffet/**", "/userPiatto/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/", "/index", "/login", "/register", "/css/**", "/images/**", "/userFiliali", "/userFiliale/**", 
+                		 "/userModello/**", "/userVersione/**").permitAll()
                 // chiunque (autenticato o no) può mandare richieste POST al punto di accesso per login e register 
                 .antMatchers(HttpMethod.POST, "/login", "/register").permitAll()
                 // solo gli utenti autenticati con ruolo ADMIN possono accedere a risorse con path /admin/**
